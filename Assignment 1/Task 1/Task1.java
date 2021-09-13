d36ef22ac75f92a3505135316692f5ec24a525b4
Dawood Chattha <87930468+DawoodSarfrazChattha@users.noreply.github.com>
2021-09-13 11:17:53 -0700

    Create Task1.java

---
+++ Assignment 1/Task 1/Task1.java
@@ -0,0 +1,18 @@
+package com.chattha;
+
+public class Task1 {
+    public static void main(String[] args)
+    {
+        double Energy , mass , speedOfLight;
+
+        mass = 20;
+
+        speedOfLight = 30000 * 1000;
+
+        Energy = mass * speedOfLight;
+
+        System.out.println("Enter Mass in kilograms : " + mass + " Kg " );
+        System.out.println("EnergyProduced : " + Energy );
+
+    }
+}
