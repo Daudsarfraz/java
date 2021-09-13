69cba3fae9f717fcb641a1f67240516c181a2f2b
Dawood Chattha <87930468+DawoodSarfrazChattha@users.noreply.github.com>
2021-09-13 11:18:45 -0700

    Create Task2.java

---
+++ Assignment 1/Task 2/Task2.java
@@ -0,0 +1,21 @@
+package com.chattha;
+
+public class Task2 {
+        public static void main(String[] args)
+
+        {
+            double oneDegree , timeTaken , angleInDegree;
+
+            oneDegree = 240/60;
+
+            angleInDegree = 16.18;
+
+            timeTaken = oneDegree * angleInDegree;
+
+            System.out.println("Enter the angle in degree = " + angleInDegree);
+
+            System.out.println("time = " + timeTaken + " minutes");
+
+
+        }
+}
