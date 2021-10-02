1cd0dca4a516242c4e3370cbbe31f0c919036a8c
Dawood Chattha <87930468+DawoodSarfrazChattha@users.noreply.github.com>
2021-10-02 12:50:58 +0500

    Update Task1.java

--- Assignments/Assignment 2/Task 1/Task1.java
+++ Assignments/Assignment 2/Task 1/Task1.java
@@ -1,1 +1,21 @@
+	public class Task1
+	{
+	public static void main(String[] args)
+	
+	{
+		double oneDegree , timeTaken , angleInDegree;
 
+		oneDegree = 240/60;
+
+		angleInDegree = 16.18;
+
+		timeTaken = oneDegree * angleInDegree;
+
+		System.out.println("Enter the angle in degree = " + angleInDegree);
+		
+		System.out.println("time = " + timeTaken + " minutes");
+
+		
+	}
+	
+	}
