76cdb0389c6d8e27f84c7a906746427ee3178f05
Dawood Chattha <87930468+DawoodSarfrazChattha@users.noreply.github.com>
2021-10-02 12:49:43 +0500

    Update Task5.java

--- Assignments/Assignment 1/Task 5/Task5.java
+++ Assignments/Assignment 1/Task 5/Task5.java
@@ -1,1 +1,20 @@
+	public class Task5
+	
+{
+	public static void main(String[] args)
+	{
+		double Energy , mass , speedOfLight;
+		
+		mass = 20;
 
+		speedOfLight = 30000 * 1000;
+		
+		Energy = mass * speedOfLight;
+
+		//System.out.println("Enter Mass in kilograms : " + mass + " Kg " );
+		//System.out.println("EnergyProduced : " + Energy );
+		System.out.println("Dawood");
+	
+	}
+	
+	}
