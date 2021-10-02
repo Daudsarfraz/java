39f154efe745df95bc56a4e23a7f0b0b06fcfa06
Dawood Chattha <87930468+DawoodSarfrazChattha@users.noreply.github.com>
2021-10-02 12:54:38 +0500

    Update Task4.java

--- Assignments/Assignment 2/Task 4/Task4.java
+++ Assignments/Assignment 2/Task 4/Task4.java
@@ -1,1 +1,20 @@
-
+	import java.util.Scanner;
+	public class Task4
+	{
+    public static void main(String[] args)
+    {
+	double gas,mpg;    
+	Scanner in = new Scanner(System.in);
+    	System.out.println("Enter the number of gallons of gas in the tank ");
+    	gas = in.nextDouble();
+	Scanner key = new Scanner(System.in);
+    	System.out.println("Enter the fuel efficiency ");
+	mpg = in.nextDouble();
+	double COST_PER_GALLON = 3.95;
+    	double cost = (100 / mpg) * COST_PER_GALLON;  
+    	System.out.print("Cost:");
+    	System.out.printf("%12.2f\n", cost);
+    	
+         }
+	 
+	}
