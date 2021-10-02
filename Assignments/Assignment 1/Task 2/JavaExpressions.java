9ea155ea433b97efc9913079219023d9a59bee08
Dawood Chattha <87930468+DawoodSarfrazChattha@users.noreply.github.com>
2021-10-02 12:45:12 +0500

    Update and rename Task2.java to JavaExpressions.java

---
+++ Assignments/Assignment 1/Task 2/JavaExpressions.java
@@ -0,0 +1,14 @@
+public class JavaExpressions{
+	public static void main(String[] args){
+	System.out.println("s = s0 + v0*t + (1 / 2)*g*Math.pow(t , 2)");
+	System.out.println("FV = PV * Math.pow(1 + INT/100, YRS)");
+	System.out.println("G = 4 * Math.pow(Math.PI , 2)*(Math.pow(a , 3)/Math.pow(p , 2)*(m1 + m2))");
+	System.out.println("c = Math.sqrt(Math.pow(a , 2)+Math.pow(b , 2)-2*a*b*Math.cos(y))");
+
+	}//main 
+
+
+
+
+
+}//class
