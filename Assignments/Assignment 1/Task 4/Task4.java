2de8ada7365bcd7b1fd911e281060aa4692d765a
Dawood Chattha <87930468+DawoodSarfrazChattha@users.noreply.github.com>
2021-10-02 12:48:44 +0500

    Update Task4.java

--- Assignments/Assignment 1/Task 4/Task4.java
+++ Assignments/Assignment 1/Task 4/Task4.java
@@ -1,1 +1,31 @@
+	import java.util.Scanner;
+	public class Task4
+	{
+	public static void main(String[] args)
+	{
+		String Name;
+		int Age;
+		String city;
+		String collegeName;
+		String Profession;
 
+		Scanner key = new Scanner(System.in);
+		
+		System.out.println("Enter your Name : ");
+		Name = key.nextLine();
+
+		System.out.println("Enter your Age : ");
+		Age = key.nextInt();
+
+		System.out.println("Enter your City : ");
+		city = key.nextLine();
+
+		System.out.println("Enter your College Name : ");
+		collegeName = key.nextLine();
+
+		System.out.println("Enter your Profession : ");
+		Profession = key.nextLine();
+
+		System.out.println("There was once a person named " + Name + " who lived in " + city + ".At the age of " + Age + " , " + Name + " went to college at " + collegeName + " , " + Name + " graduated " + " and went to work as a " + Profession + " . ");
+	}
+	} 
